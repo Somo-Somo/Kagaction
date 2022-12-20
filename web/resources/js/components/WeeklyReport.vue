02a0c2524c09a1a90192fd8d84b1b57b07295abf
shitogishi_yujin <hyerk.master@gmail.com>
2022-12-20 15:54:41 +0900

    Firebase準備

--- web/resources/js/components/WeeklyReport.vue
+++ web/resources/js/components/WeeklyReport.vue
@@ -17,7 +17,43 @@
 </template>
 
 <script>
+// Import the functions you need from the SDKs you need
 import firebase from "firebase";
+import { v4 as uuidv4 } from "uuid";
+
+const config = {
+    apiKey: propcess.env.FIREBASE_API_KEY,
+    authDomain: propcess.env.FIREBASE_AUTH_DOMAIN,
+    projectId: propcess.env.FIREBASE_PROJECT_ID,
+    storageBucket: propcess.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: propcess.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: propcess.env.FIREBASE_APP_ID,
+    measurementId: propcess.env.FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(config);
+const db = firebase.firestore();
+
+function svg2imageData(svgElement, successCallback, errorCallback) {
+    var canvas = document.createElement("canvas");
+    canvas.width = 1200;
+    canvas.height = 630;
+    var ctx = canvas.getContext("2d");
+    var image = new Image();
+    image.onload = () => {
+        ctx.drawImage(image, 0, 0, 1200, 630);
+        successCallback(canvas.toDataURL());
+    };
+    image.onerror = (e) => {
+        errorCallback(e);
+    };
+    var svgData = new XMLSerializer().serializeToString(svgElement);
+    image.src =
+        "data:image/svg+xml;charset=utf-8;base64," +
+        btoa(unescape(encodeURIComponent(svgData)));
+}
+
 export default {
     props: {
         data: {
@@ -27,6 +63,37 @@ export default {
     computed: {
         data: function () {
             return axios.post("/api/debug", "abc");
+        },
+    },
+    methods: {
+        create() {
+            // refでsvgCardをsvgに設定しているのでthis.$refs.svgCardで要素を取れます
+            svg2imageData(this.$refs.svgCard, (data) => {
+                const sRef = firebase.storage().ref();
+                const fileRef = sRef.child(`${uuid}.png`);
+                const uuid = uuidv4();
+
+                // Cloud Storageにアップロード
+                fileRef
+                    .putString(data, "data_url")
+                    .then((snapshot) => {
+                        // Firestoreに保存しておく
+                        const card = db.collection("cards").doc(uuid);
+
+                        return card.set(
+                            {
+                                message: this.description,
+                            },
+                            { merge: false }
+                        );
+                    })
+                    .then((docRef) => {
+                        console.log(docRef);
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                    });
+            });
         },
     },
 };
