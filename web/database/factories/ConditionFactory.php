70483040e216d2626a100993268ab4ab41dfa61f
shitogishi_yujin <hyerk.master@gmail.com>
2023-01-09 15:08:30 +0900

    factory使ってseederでテストデータ入れました

---
+++ web/database/factories/ConditionFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Condition;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
+
+class ConditionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Condition::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'evaluation' => rand(1, 5),
+            'date' => Carbon::yesterday(),
+            'time' => date('H:i:s'),
+        ];
+    }
+}
---
+++ web/database/factories/FeelingFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Feeling;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
+
+class FeelingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Feeling::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $feelings = [
+            'anxious',
+            'hard',
+            'tired',
+            'sad',
+            'angry',
+            'kuyashi',
+            'lethargic',
+            'moyamoya',
+            'glad',
+            'fun',
+            'calm',
+            'happy',
+            'wakuwaku',
+        ];
+        $feeling = $feelings[rand(0, count($feelings) - 1)];
+        return [
+            'feeling_type' => $feeling,
+            'date' => Carbon::yesterday(),
+            'time' => date('H:i:s'),
+        ];
+    }
+}
